69e18a6bb0aa2d1c9beb3da6c4cfac11d741da4b
jrulloa <78035294+jrulloa@users.noreply.github.com>
2021-04-30 17:53:51 -0400

    Update QuickDBD-Free Diagram (1).sql

--- QuickDBD-Free Diagram (1).sql
+++ QuickDBD-Free Diagram (1).sql
@@ -11,20 +11,21 @@ CREATE TABLE "Netflix_Movies" (
     "cast" varchar   NOT NULL,
     "country" varchar   NOT NULL,
     "date_added" Date   NOT NULL,
-    "release_year" int(4)   NOT NULL,
+    "release_year" int   NOT NULL,
     "rating" varchar   NOT NULL,
-    "duration" int(4)   NOT NULL,
+    "duration" int   NOT NULL,
     "listed_in" varchar   NOT NULL,
     "description" varchar   NOT NULL,
-    "imdb_rating" varchar   NOT NULL,
-    "imdb_votes" varchar   NOT NULL,
-    "hulu_stream" boolean   NOT NULL,
-    "prime_video_stream" boolean   NOT NULL,
-    "Disney+_stream" boolean   NOT NULL,
+    "imdb" varchar   NOT NULL,
+    "rotten tomatoes" varchar   NOT NULL,
+    "hulu" boolean   NOT NULL,
+    "prime_video" boolean   NOT NULL,
+    "Disney+" boolean   NOT NULL,
     CONSTRAINT "pk_Netflix_Movies" PRIMARY KEY (
         "show_id"
      )
 );
+
 
 CREATE TABLE "Countries" (
     "country" varchar   NOT NULL,
